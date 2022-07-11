0759d1261b41732ce3da0af74cf2945500547541
Kimilm <gnlfla7416@gmail.com>
2022-07-11 19:34:02 +0900

    회원가입 기능 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 devmongo = 'mongodb+srv://test:sparta@cluster0.stp4v.mongodb.net/?retryWrites=true&w=majority'
@@ -7,12 +7,14 @@ db = client.ogamemu
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+# hashlib 모듈로 비밀번호 암호화
+import hashlib
 
 
+# 회원 로그인 / 가입 / 탈퇴
+# 로그아웃은 내가 가진 토큰을 쿠키에서 지우기
+
+# 로그인 api
 @app.route("/api/login", methods=["POST"])
 def login():
     id_receive = request.form['user_id']
@@ -24,6 +26,20 @@ def login():
         return jsonify({'msg': '유저 존재하지 않음'})
 
     return jsonify({'user': user})
+
+
+# 회원가입 api
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['user_id']
+    pw_receive = request.form['user_pw']
+    birthday_receive = request.form['user_birthday']
+    # hashlib 함수로 비밀번호 암호화
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.user.insert_one({'user_id': id_receive, 'user_pw': pw_hash, 'user_birthday': birthday_receive})
+
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
