7f7bb6f371813d3808517d53df049bf05b760599
Kimilm <gnlfla7416@gmail.com>
2022-07-11 19:35:29 +0900

    메인 화면 JWT 토큰 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for, redirect
 from pymongo import MongoClient
 
 devmongo = 'mongodb+srv://test:sparta@cluster0.stp4v.mongodb.net/?retryWrites=true&w=majority'
@@ -7,8 +7,36 @@ db = client.ogamemu
 
 app = Flask(__name__)
 
+### for JWT
+# JWT 토큰을 만들 때 필요한 비밀문자열
+SECRET_KEY = 'OGAMEMU'
+
+# JWT 패키지를 사용, PyJWT 패키지 설치
+import jwt
+
+# datetime 모듈로 토큰 만료시간 설정
+import datetime
+
 # hashlib 모듈로 비밀번호 암호화
 import hashlib
+
+
+@app.route('/')
+def home():
+    # 쿠키에 저장된 jwt 토큰을 가져온다
+    token_receive = request.cookies.get('mytoken')
+    try:
+        # 서버에 지정된 비밀 문자열로 토큰을 해석한다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 해석된 토큰값에서 id를 가져온다.
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template('index.html', nickname=user_info["nick"])
+    # 토큰 시간이 만료되었다면
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    # 비밀 문자열이 틀리거나 하는 이유로 해석에 실패했다면
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
 # 회원 로그인 / 가입 / 탈퇴
