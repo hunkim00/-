d6c95139ce7a3814917e30857552bae8e44b570b
hunkim00 <kimhun1027@naver.com>
2022-07-12 14:42:57 +0900

    app.py 및 templates 업데이트

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify, render_template, url_for, redirect
 from pymongo import MongoClient
 
-devmongo = 'mongodb+srv://test:sparta@cluster0.stp4v.mongodb.net/?retryWrites=true&w=majority'
+import requests
+from bs4 import BeautifulSoup
+
+devmongo = 'mongodb+srv://admin:admin@cluster0.h3znj.mongodb.net/?retryWrites=true&w=majority'
 client = MongoClient(devmongo)
 db = client.jwtTest
+
+client = MongoClient('mongodb+srv://admin:admin@cluster0.h3znj.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 
 app = Flask(__name__)
 
@@ -99,8 +106,6 @@ def api_check_id():
     else:
         return jsonify({'result': 'success', 'msg': '사용 가능한 아이디 입니다.'})
 
-
-
 # 회원가입 api
 @app.route('/api/signup', methods=['POST'])
 def api_signup():
@@ -113,6 +118,73 @@ def api_signup():
     db.user.insert_one({'user_id': id_receive, 'user_pw': pw_hash, 'user_birthday': birthday_receive})
 
     return jsonify({'result': 'success'})
+
+#@app.route('/')
+#def home():
+#   return render_template('index.html')
+
+@app.route("/game", methods=["POST"])
+def game_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+    price_receive = request.form['price_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    import requests
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+
+    image = soup.select_one('body > app-root > div > app-game-overview > div > div.header-image-container > picture > source:nth-child(2)')['srcset']
+    title = soup.select_one(
+        'body > app-root > div > app-game-overview > app-page-container-right-nav > div > div.d-flex.mb-4 > div.flex-grow-1 > div.card.mt-4 > div > h1').text.strip()
+    maker = soup.select_one(
+        'body > app-root > div > app-game-overview > app-page-container-right-nav > div > div.d-flex.mb-4 > div.flex-grow-1 > div.card.mt-4 > div > div > div.col-lg-7 > div.companies > span').text.strip()
+    date = soup.select_one(
+        'body > app-root > div > app-game-overview > app-page-container-right-nav > div > div.d-flex.mb-4 > div.flex-grow-1 > div.card.mt-4 > div > div > div.col-lg-7 > div.platforms').text[
+           0:13].strip()
+    platform = soup.select_one(
+        'body > app-root > div > app-game-overview > app-page-container-right-nav > div > div.d-flex.mb-4 > div.flex-grow-1 > div.card.mt-4 > div > div > div.col-lg-7 > div.platforms').text[
+            14:1000].strip()
+    game_list =  list(db.games.find({}, {'_id': False}))
+    count = len(game_list) +1
+
+    doc = { 'title': title,
+            'image':image,
+            'maker': maker,
+            'star': star_receive,
+            'comment': comment_receive,
+            'date' : date,
+            'platform' : platform,
+            'price' : price_receive,
+            'num': count
+    }
+    db.games.insert_one(doc)
+
+    return jsonify({'msg':'작성완료!'})
+
+@app.route("/game", methods=["GET"])
+def game_get():
+    games_list = list(db.games.find({}, {'_id': False}))
+    return jsonify({'list': games_list})
+
+# @app.route("/game/change", methods=["POST"])
+# def game_change():
+#     comment_receive = request.form['comment_give']
+#     star_receive = request.form['star_give']
+#     num_receive = request.form['num_give']
+#     db.games.update_one({'name':  num_receive}, {'$set': {'comment': comment_receive}})
+#     db.games.update_one({'name':  num_receive}, {'$set': {'star': star_receive}})
+#     return jsonify({'msg': '변경완료'})
+
+@app.route("/game/delete", methods=["POST"])
+def game_change():
+    num_receive = request.form['num_give']
+    db.games.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제완료'})
 
 
 if __name__ == '__main__':
